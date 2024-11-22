f819379db92080a7a3ab6aa529a462b1a131d98e
simantoz <mfadhlurrahman18@gmail.com>
2024-11-22 15:00:38 +0700

    ini tugas perkenalan

---
+++ package main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string = "nama : Muhammad Fadhlurrahman"
+	var nim string = "nim : 103062400091"
+	var kelas string = "kelas : 24-002"
+	fmt.Println(nama)
+	fmt.Println(nim)
+	fmt.Println(kelas)
+}
